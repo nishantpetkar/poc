80ee25d728083e96ca580e225eb6494e1eb21bb1
Nishant Petkar <nishant.petkar@intangles.com>
2020-07-01 15:12:21 +0530

    modifications for api usage

--- maps.js
+++ maps.js
@@ -1,26 +1,77 @@
-// const decode = require('geojson-polyline').decode;
-// var data = require("./maa-chirala.json");
-// const h3 = require("h3-js");
-var geohash = require("ngeohash");
-var fs = require("fs");
+"use strict";
+
+const decode = require('geojson-polyline').decode;
+const https = require("https");
+const geohash = require("ngeohash");
+const fs = require("fs");
+
+/* origin and destination expected in (lat,long) format or other google api compatible format */
+async function createRoute(origin, destination) {
+    return new Promise((resolve, reject) => {
+        var url = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&sensor=false&mode=driving&key=AIzaSyB8DUoCptnmYmJWyqY6c8UgFIJdgp5_Lk0`;
+        https.get(url, (resp) => {
+            let data = '';
+
+            resp.on("data", (chunk) => {
+                data += chunk;
+            });
+
+            resp.on("end", async() => {
+                await dataToLatLong(JSON.parse(data));
+                resolve();
+            });
+        }).on("error", (err) => {
+            console.log("Error: " + err.message);
+            reject(err.message);
+        });
+    });
+}
 
 /* polyline data to lat long */
-// for(var i = 0; i < data.routes[0].legs[0].steps.length; i++) {
-//     const polygon = {
-//         type: 'Polygon',
-//         coordinates: [data.routes[0].legs[0].steps[i].polyline.points]
-//     };
-//     fs.appendFileSync("./maa-chirala-latlng.txt", "\n"+decode(polygon).coordinates[0]);
-// }
+async function dataToLatLong(data) {
+    return new Promise(async(resolve, reject) => {
+        var lat_long = '';
+        try {
+            for(var i = 0; i < data.routes[0].legs[0].steps.length; i++) {
+                const polygon = {
+                    type: 'Polygon',
+                    coordinates: [data.routes[0].legs[0].steps[i].polyline.points]
+                };
+                if(i !== 0) {
+                    lat_long += ("," + decode(polygon).coordinates[0]);
+                }
+                else {
+                    lat_long += decode(polygon).coordinates[0];
+                }
+            }
+            await LatLongToGeohash(lat_long);
+            resolve();
+        } catch(err) {
+            console.log(err);
+            reject(err);
+        }
+    });
+}
 
 /* lat long to h3 */
-var str = fs.readFileSync("./maa-chirala-latlng.txt").toString().split(",");
+async function LatLongToGeohash(data) {
+    return new Promise((resolve, reject) => {
+        try {
+            var str = data.toString().split(",");
+            var geohash_array = [];
+            for(var i = 0; i < str.length; i += 2) {
+                geohash_array.push(geohash.encode(parseFloat(str[i+1]), parseFloat(str[i]), 6));
+            }
+            var geohashes = Array.from(new Set(geohash_array));
 
-var geohash_array = [];
-for(var i = 0; i < str.length; i += 2) {
-    geohash_array.push(geohash.encode(parseFloat(str[i+1]), parseFloat(str[i]), 6));
+            console.log(geohashes.length);
+            fs.writeFileSync("./route-geohash.csv", geohashes);
+            resolve();
+        } catch(err) {
+            console.log(err);
+            reject(err);
+        }
+    });
 }
-var geohashes = Array.from(new Set(geohash_array));
 
-console.log(geohashes.length);
-fs.writeFileSync("./maa-chirala-geohash.csv", geohashes);
+exports.createRoute = createRoute;
